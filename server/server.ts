12ca2b082cb3fcdc6b72934a5dedc79990bcc888
Ubuntu <howardzchung@gmail.com>
2020-05-31 13:50:36 +0000

    prettier and updates
    Former-commit-id: 4ea98619aafc9a920ce9574df993f907fe516b01

---
+++ server/server.ts
@@ -0,0 +1,81 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+import sqlite3 from 'sqlite3';
+import { open, Database } from 'sqlite';
+import cors from 'cors';
+
+let db: Database = (null as unknown) as Database;
+async function init() {
+  db = await open({ filename: './data/opencfb.sqlite', driver: sqlite3.Database });
+  // TODO run the schema.sql file
+  // db.serialize(function() {
+  //     db.run("CREATE TABLE IF NOT EXISTS message (id TEXT, depth INT, data TEXT, pubkey TEXT, sig TEXT)");
+  //     db.run("CREATE INDEX IF NOT EXISTS message_id_idx ON message(id)");
+  // });
+  app.listen(process.env.PORT || 5000);
+}
+init();
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Team {
+      id: String
+      displayName: String
+      score: Int
+      field: String
+      result: String
+  }
+  
+  type Game {
+      id: String
+      state: String
+      date: String
+      teams: [Team]
+  }
+  
+  type Query {
+    getTeam(id: String): Team
+    getGame(teamId: String, gameId: String): [Game]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  getTeam: async ({ id }: { id: string }) => {
+    const data = await db.all('SELECT id, displayName FROM team where id = ?', [
+      id,
+    ]);
+    return data[0];
+  },
+  getGame: async ({ teamId, gameId }: { teamId: string; gameId: string }) => {
+    const data = await db.all(
+      'SELECT game.id, game.state, game.date FROM game join gameteam on game.id = gameteam.gameid where gameteam.teamid = ? order by game.date desc',
+      [teamId]
+    );
+    // TODO fetch the teams that played
+    console.log(teamId, data);
+    return data;
+  },
+};
+
+var app = express();
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+// TODO serve static client files
+// TODO run update worker
+// TODO more features
+// - circles of parity
+// - all time rankings
+// - team pages
+// - all time win streaks
+// - active win streaks
+// - margins of victory
+// - rivalry games
