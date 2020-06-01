dd27201330f37cd7ad02772925b0e0612f688528
Ubuntu <howardzchung@gmail.com>
2020-06-01 10:33:01 +0000

    move code around, remove submodules
    Former-commit-id: 7acf11f18bb5545385a3e54fde7d79cb27eed3a6 [formerly e364eddfc23693bde80d1f01225ac8c1743cb058]
    Former-commit-id: 98d2cac4622822dfa92ac61773eaad6575c55d81

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Query, QueryResult } from 'react-apollo';
 import gql from 'graphql-tag';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri:
@@ -12,11 +13,11 @@ const client = new ApolloClient({
       : `${window.location.protocol}//${window.location.host}/graphql`,
 });
 
-const Games = () => (
+const Game = ({ gameId }: { gameId: string }) => (
   <Query
     query={gql`
       {
-        getGame(teamId: "333") {
+        getGame(gameId: ${gameId}) {
           id
           state
           date
@@ -40,6 +41,118 @@ const Games = () => (
   </Query>
 );
 
+const Team = ({ teamId }: { teamId: string }) => (
+  <Query
+    query={gql`
+      {
+        getTeam(teamId: ${teamId}) {
+          id
+          displayName
+        }
+      }
+    `}
+  >
+    {(result: QueryResult) => {
+      const { loading, error, data } = result;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    }}
+  </Query>
+);
+
+const TeamGames = ({ teamId }: { teamId: string }) => (
+  <Query
+    query={gql`
+      {
+        getGame(teamId: ${teamId}) {
+          id
+          state
+          date
+          teams {
+            id
+            score
+            field
+            result
+          }
+        }
+      }
+    `}
+  >
+    {(result: QueryResult) => {
+      const { loading, error, data } = result;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    }}
+  </Query>
+);
+
+const Games = () => (
+  <Query
+    query={gql`
+      {
+        listGame(limit: 10) {
+          id
+          state
+          date
+        }
+      }
+    `}
+  >
+    {(result: QueryResult) => {
+      const { loading, error, data } = result;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    }}
+  </Query>
+);
+
+const Teams = () => (
+  <Query
+    query={gql`
+      {
+        listTeam(limit: 10) {
+          id
+          displayName
+        }
+      }
+    `}
+  >
+    {(result: QueryResult) => {
+      const { loading, error, data } = result;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return <pre>{JSON.stringify(data, null, 2)}</pre>;
+    }}
+  </Query>
+);
+
+const Home = () => (
+  <div>
+    <Teams />
+    <Games />
+  </div>
+);
+
+const TeamPage = ({ teamId }: { teamId: string }) => (
+  <div>
+    <Team teamId={teamId} />
+    <TeamGames teamId={teamId} />
+  </div>
+);
+
+const GamePage = ({ gameId }: { gameId: string }) => (
+  <div>
+    <Game gameId={gameId} />
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
@@ -48,7 +161,23 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">OpenCFB</h1>
           </header>
-          <Games />
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" render={() => <Home />} />
+              <Route
+                path="/teams/:teamId?"
+                render={({ match }) => (
+                  <TeamPage teamId={match.params.teamId} />
+                )}
+              />
+              <Route
+                path="/games/:gameId?"
+                render={({ match }) => (
+                  <GamePage gameId={match.params.gameId} />
+                )}
+              />
+            </Switch>
+          </BrowserRouter>
         </div>
       </ApolloProvider>
     );
