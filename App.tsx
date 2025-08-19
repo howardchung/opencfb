713b6136c81d6423d6cf42b21412ef0f49e47594
Howard Chung <howardzchung@gmail.com>
2025-08-19 01:16:58 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -54,7 +54,7 @@ async function loadWorker() {
         from: 'inline',
         config: {
           serverMode: 'full',
-          url: process.env.NODE_ENV === 'development' ? '/opencfb.sqlite' : 'https://howardchung.github.io/opencfb/public/opencfb.sqlite',
+          url: process.env.NODE_ENV === 'development' ? '/opencfb.sqlite' : /opencfb.sqlite',
           requestChunkSize: 4096,
         },
       },
